6fd0a7a551f9af4ee14e3716938a86ed766f1401
joshuaiyumva <joshuaiyumva@gmail.com>
2020-11-15 16:36:28 +0200

    add repository class

---
+++ src/app/repository.ts
@@ -0,0 +1,5 @@
+export class Repository {
+    constructor(public repo_name: string, public html_url: string, public repo_desc: string, public created_on: Date
+        , public repo_language: string) {
+    }
+}
